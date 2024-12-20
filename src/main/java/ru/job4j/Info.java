3d535717e78371d1c87af3e895636f5d0f33259a
dmitrii <dbalan@okta-solutions.com>
2024-12-20 12:56:00 +0300

    Add file Info with today date

---
+++ src/main/java/ru/job4j/Info.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import java.time.LocalDate;
+
+public class Info {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        System.out.println("Today is: " + localDate);
+    }
+}
