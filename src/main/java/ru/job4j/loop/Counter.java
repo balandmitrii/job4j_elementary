259b07dfe903d7850f730038b583156c0e45f6c7
dmitrii <dbalan@okta-solutions.com>
2024-12-21 20:54:36 +0300

    добавлен метод sumByEven с тестами для него

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -15,6 +15,22 @@ public class Counter {
         return sum;
     }
 
+    public static int sumByEven(int start, int finish) {
+        if (finish <= start) {
+            return 0;
+        }
+
+        int sum = 0;
+
+        for (int i = start; i < finish; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(1, 20));
         System.out.println(sum(12, 2));
--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -32,4 +32,31 @@ class CounterTest {
         int output = Counter.sum(start, finish);
         assertThat(output).isEqualTo(expected);
     }
+
+    @Test
+    void whenSumByEvenStart1AndFinish5Than6() {
+        int start = 1;
+        int finish = 5;
+        int expected = 6;
+        int output = Counter.sumByEven(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenStart0AndFinish25Than156() {
+        int start = 0;
+        int finish = 25;
+        int expected = 156;
+        int output = Counter.sumByEven(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumByEvenStartMinus15AndFinish5ThanMinus50() {
+        int start = -15;
+        int finish = 5;
+        int expected = -50;
+        int output = Counter.sumByEven(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
 }
