e1f325f8cfe208ba4b014780fe4f799049727ce7
dmitrii <dbalan@okta-solutions.com>
2024-12-22 20:48:59 +0300

    В классе ArrayDefinition добавлен массив names и вывод каждого из значений

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages: " + ages.length);
         System.out.println("Размер массива surnames: " + surnames.length);
         System.out.println("Размер массива prices: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Александр Македонский";
+        names[1] = "Фабий Максим";
+        names[2] = "Сципион Африканский";
+        names[3] = "Гораций Кокл";
+        System.out.println("Names[0]: " + names[0]);
+        System.out.println("Names[1]: " + names[1]);
+        System.out.println("Names[2]: " + names[2]);
+        System.out.println("Names[3]: " + names[3]);
     }
 }
