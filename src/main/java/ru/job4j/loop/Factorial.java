3005421e0c513103f06b42b4206209ee0827ef59
dmitrii <dbalan@okta-solutions.com>
2024-12-22 11:35:00 +0300

    удалить лишний IF и пробелмы между строками в метода factorial

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -2,16 +2,10 @@ package ru.job4j.loop;
 
 public class Factorial {
     public static int factorial(int number) {
-        if (number <= 1) {
-            return 1;
-        }
-
         int result = 1;
-
-        for (int i = 1; i <= number; i++) {
+        for (int i = 2; i <= number; i++) {
             result *= i;
         }
-
         return result;
     }
 }
