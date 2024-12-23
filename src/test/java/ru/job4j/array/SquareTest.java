28453510002225206adadd21a3bb768d9ccf3d2e
dmitrii <dbalan@okta-solutions.com>
2024-12-23 13:17:23 +0300

    Заполнить массив степенями чисел

---
+++ src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SquareTest {
+
+    @Test
+    @DisplayName("Сравнить массив {0, 1, 4} для 3 чисел")
+    void whenCalculateArrayFor3Than0And1And4() {
+        int[] expected = {0, 1, 4};
+        int[] output = Square.calculate(3);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Сравнить массив {0} для 1 числа")
+    void whenCalculateArrayFor1Than0() {
+        int[] expected = {0};
+        int[] output = Square.calculate(1);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Сравнить массив {0, 1, 4, 9} для 4 чисел")
+    void whenCalculateArrayFor4Than0And1And4And9() {
+        int[] expected = {0, 1, 4, 9};
+        int[] output = Square.calculate(4);
+        assertThat(output).isEqualTo(expected);
+    }
+}
