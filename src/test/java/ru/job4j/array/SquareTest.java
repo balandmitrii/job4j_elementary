281828e4d10c4bfd3fa1953f56f38964e0bcca9e
dmitrii <dbalan@okta-solutions.com>
2024-12-23 13:23:50 +0300

    Дополнить тесты для Square.calculate для 5 чисел, использовать метод containsExactly

--- src/test/java/ru/job4j/array/SquareTest.java
+++ src/test/java/ru/job4j/array/SquareTest.java
@@ -12,7 +12,7 @@ class SquareTest {
     void whenCalculateArrayFor3Than0And1And4() {
         int[] expected = {0, 1, 4};
         int[] output = Square.calculate(3);
-        assertThat(output).isEqualTo(expected);
+        assertThat(output).containsExactly(expected);
     }
 
     @Test
@@ -20,7 +20,7 @@ class SquareTest {
     void whenCalculateArrayFor1Than0() {
         int[] expected = {0};
         int[] output = Square.calculate(1);
-        assertThat(output).isEqualTo(expected);
+        assertThat(output).containsExactly(expected);
     }
 
     @Test
@@ -28,6 +28,14 @@ class SquareTest {
     void whenCalculateArrayFor4Than0And1And4And9() {
         int[] expected = {0, 1, 4, 9};
         int[] output = Square.calculate(4);
-        assertThat(output).isEqualTo(expected);
+        assertThat(output).containsExactly(expected);
+    }
+
+    @Test
+    @DisplayName("Сравнить массив {0, 1, 4, 9, 16} для 5 чисел")
+    void whenCalculateArrayFor5Than0And1And4And9And16() {
+        int[] expected = {0, 1, 4, 9, 16};
+        int[] output = Square.calculate(5);
+        assertThat(output).containsExactly(expected);
     }
 }
