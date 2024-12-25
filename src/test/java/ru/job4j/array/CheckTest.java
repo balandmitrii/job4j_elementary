2d6a06894ad522497d98c797565b9a19676e4304
dmitrii <dbalan@okta-solutions.com>
2024-12-25 12:52:13 +0300

    Массив заполнен true или false

--- src/test/java/ru/job4j/array/CheckTest.java
+++ src/test/java/ru/job4j/array/CheckTest.java
@@ -4,19 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CheckTest {
-    @Test
-    void whenDataMonoByTrueThenTrue() {
-        boolean[] data = new boolean[] {true, true, true};
-        boolean result = Check.mono(data);
-        assertThat(result).isTrue();
-    }
-
-    @Test
-    void whenDataNotMonoByTrueThenFalse() {
-        boolean[] data = new boolean[] {true, false, true};
-        boolean result = Check.mono(data);
-        assertThat(result).isFalse();
-    }
 
     @Test
     void whenDataMonoByFalseThenTrue() {
@@ -28,6 +15,20 @@ class CheckTest {
     @Test
     void whenDataNotMonoByFalseThenFalse() {
         boolean[] data = new boolean[] {false, true, false};
+        boolean result = Check.mono(data);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void whenDataMonoByTrueThenTrue() {
+        boolean[] data = new boolean[] {true, true, true};
+        boolean result = Check.mono(data);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void whenDataNotMonoByTrueThenFalse() {
+        boolean[] data = new boolean[] {true, false, true};
         boolean result = Check.mono(data);
         assertThat(result).isFalse();
     }
